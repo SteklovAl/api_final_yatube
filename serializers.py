faa725ea644215c6602e1811b46c1e7bce5e9864
SteklovAl <104057209+SteklovAl@users.noreply.github.com>
2022-08-08 18:42:59 +0500

    Add files via upload

---
+++ serializers.py
@@ -0,0 +1,58 @@
+from posts.models import Comment, Follow, Group, Post, User
+from rest_framework import serializers
+from rest_framework.relations import SlugRelatedField
+from rest_framework.validators import UniqueTogetherValidator
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = SlugRelatedField(slug_field='username', read_only=True)
+
+    class Meta:
+        fields = '__all__'
+        model = Post
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username'
+    )
+
+    class Meta:
+        fields = '__all__'
+        model = Comment
+
+
+class GroupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        fields = '__all__'
+        model = Group
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True,
+        default=serializers.CurrentUserDefault()
+    )
+
+    following = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all()
+    )
+
+    class Meta:
+        model = Follow
+        fields = ('user', 'following')
+        validators = (
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=('user', 'following')
+            ),
+        )
+
+    def validate_following(self, following):
+        if self.context.get('request').user == following:
+            raise serializers.ValidationError(
+                'Нельзя подписаться на самого себя')
+        return following
---
+++ urls.py
@@ -0,0 +1,20 @@
+from django.urls import include, path
+from rest_framework import routers
+
+from .views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
+
+app_name = 'api'
+
+v1_router = routers.DefaultRouter()
+v1_router.register(r'posts', PostViewSet, basename='posts')
+v1_router.register(r'groups', GroupViewSet, basename='groups')
+v1_router.register(r'posts/(?P<post_id>\d+)/comments',
+                   CommentViewSet, basename='comments')
+v1_router.register(r'follow', FollowViewSet, basename='follow')
+
+
+urlpatterns = [
+    path('v1/', include('djoser.urls')),
+    path('v1/', include('djoser.urls.jwt')),
+    path('v1/', include(v1_router.urls)),
+]
